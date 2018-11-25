f0d9eed80b6918fb33c088302ac74a5a88d21de9
Arvids Gargurnis <arvids.gargurnis@plymouth.ac.uk>
2018-11-25 14:24:16 +0000

    Implemented validation on login and register fields

--- client/javascript/auth.js
+++ client/javascript/auth.js
@@ -1,3 +1,31 @@
+class Observable {
+    // when instantiated create an empty observer array
+    constructor() {
+        this.observers = [];
+    }
+    // function to add new observers to the array
+    subscribe(o) {
+        this.observers.push(o);
+    }
+    // function to remove observer from array if needed
+    unsubscribe(o) {
+        this.observers = this.observers.filter(subscriber => subscriber !== o);
+    }
+    // function that notifies all the observers of an action that needs to be done
+    notify(action) {
+        if (action == 'enable') {
+            this.observers.forEach(observer => (
+                observer.classList.add('btn-primary'),
+                observer.classList.remove('btn-secondary')
+            ));
+        } else if (action == 'disable') {
+            this.observers.forEach(observer => (
+                observer.classList.remove('btn-primary'),
+                observer.classList.add('btn-secondary')
+            ));
+        }
+    }
+}
 class Player {
     constructor(username) {
         this.username = username;
@@ -10,6 +38,10 @@ class Player {
 const player = new Player('temp');
 
 $(document).ready(function () {
+    // instantiate new Observer class
+    const fieldObserver = new Observable();
+    // query dom for validation field
+    var validationField = $('.invalid')[0];
     // query dom containers
     var loginForm = $('#login-form')[0];
     var registerForm = $('#register-form')[0];
@@ -22,14 +54,19 @@ $(document).ready(function () {
     var passwordInput = $('#password-input')[0];
     var usernameRegisterInput = $('#username-register-input')[0];
     var passwordRegisterInput = $('#password-register-input')[0];
+    // add buttons to observer array
+    fieldObserver.subscribe(registerUserBtn);
+    fieldObserver.subscribe(loginBtn);
     // welcome container listeners
     registerFormBtn.addEventListener('click', function () {
         registerForm.classList.remove('hidden');
         loginForm.classList.add('hidden');
     })
     registerUserBtn.addEventListener('click', function () {
-        registerForm.classList.add('hidden');
-        loginForm.classList.remove('hidden');
+        if ($('#register-btn').hasClass('btn-primary')) {
+            registerForm.classList.add('hidden');
+            loginForm.classList.remove('hidden');
+        }
     })
     // click the login button on the press of the enter key in username input field
     usernameInput.addEventListener("keyup", function (e) {
@@ -38,12 +75,24 @@ $(document).ready(function () {
         if (e.keyCode === 13) {
             loginBtn.click();
         }
+        // check if both input fields have a value
+        if (usernameInput.value !== '' && passwordInput.value !== '') {
+            fieldObserver.notify('enable');
+        } else {
+            fieldObserver.notify('disable');
+        }
     });
     // click the login button on the press of the enter key in password input field
     passwordInput.addEventListener("keyup", function (e) {
         e.preventDefault();
         if (e.keyCode === 13) {
             loginBtn.click();
+        }
+        // check if both input fields have a value
+        if (usernameInput.value !== '' && passwordInput.value !== '') {
+            fieldObserver.notify('enable');
+        } else {
+            fieldObserver.notify('disable');
         }
     });
     // click the register button on the press of the enter key in username input field
@@ -52,12 +101,24 @@ $(document).ready(function () {
         if (e.keyCode === 13) {
             registerUserBtn.click();
         }
+        // check if both input fields have a value
+        if (usernameRegisterInput.value !== '' && passwordRegisterInput.value !== '') {
+            fieldObserver.notify('enable');
+        } else {
+            fieldObserver.notify('disable');
+        }
     });
     // click the register button on the press of the enter key in password input field
     passwordRegisterInput.addEventListener("keyup", function (e) {
         e.preventDefault();
         if (e.keyCode === 13) {
             registerUserBtn.click();
+        }
+        // check if both input fields have a value
+        if (usernameRegisterInput.value !== '' && passwordRegisterInput.value !== '') {
+            fieldObserver.notify('enable');
+        } else {
+            fieldObserver.notify('disable');
         }
     });
     // successful login function
@@ -66,35 +127,49 @@ $(document).ready(function () {
         localStorage.setItem(player.username, JSON.stringify(player));
         window.location.href = "http://localhost:4000/lobby?name=" + player.username;
     }
+    // unsuccessful login function
+    function invalidCredentials() {
+        validationField.classList.remove('hidden');
+    }
     // Register 
     registerUser = () => {
-        var userData = {
-            "username": usernameRegisterInput.value.toLowerCase(),
-            "password": passwordRegisterInput.value
+        if ($('#register-btn').hasClass('btn-primary')) {
+            var userData = {
+                "username": usernameRegisterInput.value.toLowerCase(),
+                "password": passwordRegisterInput.value
+            }
+            axios
+                .post('/api/auth/register', userData)
+                .then(response => {
+                    if (response.status == 200) {
+                        usernameRegisterInput.value = '';
+                        passwordRegisterInput.value = '';
+                        fieldObserver.notify('disable');
+                    }
+                })
+                .catch(error =>
+                    console.log(error)
+                );
         }
-        axios
-            .post('/api/auth/register', userData)
-            .then(response => {
-                console.log('Successfully register');
-                console.log(response);
-            })
-            .catch(error =>
-                console.log(error)
-            );
     };
     // Login
     loginUser = () => {
-        var userData = {
-            "username": usernameInput.value.toLowerCase(),
-            "password": passwordInput.value
+        if ($('#login-btn').hasClass('btn-primary')) {
+            var userData = {
+                "username": usernameInput.value.toLowerCase(),
+                "password": passwordInput.value
+            }
+            axios
+                .post('/api/auth/login', userData)
+                .then(response => {
+                    if (response.status == 200) {
+                        login();
+                    }
+                })
+                .catch(error => {
+                    invalidCredentials();
+                    console.log(error);
+                });
         }
-        axios
-            .post('/api/auth/login', userData)
-            .then(response => {
-                login();
-            })
-            .catch(error =>
-                console.log(error)
-            );
     };
 });
